42ccd850548804c7c87cfea979db1f1f2c91152e
Vduyit <vduyit@gmail.com>
2024-07-29 10:53:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,13 @@ app.use(
     credentials: true,
   })
 );
+
+// Middleware để xử lý preflight requests
+app.options('*', cors({
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://non-thao-fe.vercel.app"],
+  credentials: true,
+}));
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
