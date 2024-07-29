d759cfcd63e4b27a6f1ee6cc848be2b1522db860
Vduyit <vduyit@gmail.com>
2024-07-29 12:07:47 +0700

    Update connectDB.js

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    return (await mongoose.connect("mongodb+srv://duycute248:duycute248@cluster0.ynpjzks.mongodb.net/web_non")).then(() => console.log('Connected to MongoDB')).catch(err => console.log(err));
+    return (await mongoose.connect("mongodb+srv://duycute248:duycute248@cluster0.ynpjzks.mongodb.net/web_non"))
   } catch (error) {
     console.log("error", error);
   }
